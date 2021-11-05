c298bcf8cd280fdbd4cd442cc9647e56951bec70
ayomideio <88007511+ayomideio@users.noreply.github.com>
2021-11-05 13:11:32 +0100

    Update filter.py

--- filters/filter.py
+++ filters/filter.py
@@ -1,7 +1,5 @@
 import django_filters
 from accounts.models import User
-
-
 class UserSearchFilter(django_filters.FilterSet):
     username = django_filters.CharFilter(field_name='username', lookup_expr='icontains')
     fullname = django_filters.CharFilter(field_name='fullname', lookup_expr='icontains')
