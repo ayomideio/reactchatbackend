f6b24ead605655ce9a4ea65a1f6224682b7b4f99
ayomideio <gokeayomide.tolu@gmail.com>
2021-11-05 12:36:16 +0100

    ayomideio

--- SocialProject/settings.py
+++ SocialProject/settings.py
@@ -93,14 +93,8 @@ ASGI_APPLICATION = "SocialProject.routing.application"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-if config('DATABASE_URL', False):
-    DATABASES = {
-    'default': dj_database_url.config(
-        default = config('DATABASE_URL')
-        )
-    }
-else:
-    DATABASES = {
+
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
